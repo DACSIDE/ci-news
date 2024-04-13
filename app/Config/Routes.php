b778053387fc315c3f0f01856da59720ebfb5f38
DACSIDE <F.J.Agbonkhese@wlv.ac.uk>
2024-04-13 23:26:01 +0100

    routes codes update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@
 use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
 
-$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news', [News::class, 'index']);           //
 $routes->get('news/new', [News::class, 'new']); // Add this line
 $routes->post('news', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'show']); // Add this line
